3b3635d3e223578b54abf62be4d98bf080b562a1
hyesung99 <4538asd@gmail.com>
2023-10-05 18:01:44 +0900

    DY2-10 axiosInstance에 NirvanaLocalStorage적용

--- src/apis/axiosInstanse.ts
+++ src/apis/axiosInstanse.ts
@@ -1,10 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 import { API_BASE_URL } from '@/constants/Api';
+import NirvanaLocalStorage from '@utils/NirvanaLocalStorage';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 1000,
-  headers:{
-    Authorization: 'Bearer ' + localStorage.getItem('token')
+  headers: {
+    Authorization: 'Bearer ' + NirvanaLocalStorage.getItem('token')
   }
-})
+});
